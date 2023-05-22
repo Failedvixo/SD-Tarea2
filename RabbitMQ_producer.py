04520b8e08250ae32d64f77d2d91bca8fd35c58e
Collapsium <cristian.rodriguez_b@mail.udp.cl>
2023-05-21 22:41:10 -0400

    RabbitMQ, envia mensaje simple

--- RabbitMQ_producer.py
+++ RabbitMQ_producer.py
@@ -1,6 +1,30 @@
-import pika
-
+import pika, datetime, random, json, time
+from datetime import datetime
 #docker pull rabbitmq
+
+def producer(name):
+    trago = []
+    with open('data.txt', 'r') as data:
+        for linea in data:
+            trago.append(linea)
+
+    numero = 3
+
+    for i in range(numero):
+        numero_aleatorio = random.randint(0,len(trago)-1)
+        dt = datetime.now()
+        time_p = datetime.timestamp(dt)
+        data={
+            'name': name,
+            'timestamp': time_p,
+            'user_id': random.randint(20000, 100000),
+            'drink_name': trago[numero_aleatorio]
+        }
+
+        message = json.dumps(data)
+        channel.basic_publish(exchange='', routing_key='messages', body=message)
+        time.sleep(3)
+
 
 #Coneccion al sv
 connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
@@ -9,9 +33,5 @@ channel = connection.channel()
 #Creación cola
 channel.queue_declare(queue='messages')
 
-channel.basic_publish(exchange='',
-                      routing_key='messages',
-                      body='test')
-print(" Mensaje enviado ")
-
+producer("sex")
 connection.close()
