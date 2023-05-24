09a7b78e2c030568bf961fa15312998ded1a335f
Failedvixo <101746155+Failedvixo@users.noreply.github.com>
2023-05-23 23:08:51 -0400

    Create kafka-consumer-3topics.py

---
+++ Kafka multi topic/kafka-consumer-3topics.py
@@ -0,0 +1,44 @@
+from confluent_kafka import Consumer
+import redis
+import time
+from datetime import datetime
+################
+r = redis.Redis(host='localhost', port=6379)
+
+c=Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
+
+print('Available topics to consume: ', c.list_topics().topics)
+
+c.subscribe(['Tragos1', 'Tragos2', 'Tragos3'])
+
+
+################
+
+def main():
+    f = open("tiempos_kafka-escalabilidad-mejora.txt","w+")
+    while True:
+        msg=c.poll(1.0) #timeout
+        dt = datetime.now()
+        if msg is None:
+            continue
+        if msg.error():
+            print('Error: {}'.format(msg.error()))
+            continue
+        time_p = datetime.timestamp(dt)
+        data=msg.value().decode('utf-8')
+        print(data)
+        tiempo_final = datetime.fromtimestamp(time_p)
+        tiempo_principio = datetime.fromtimestamp(eval(data)['timestamp'])
+        tiempo_demora = tiempo_final-tiempo_principio
+        print(tiempo_demora)
+        real_data = {
+            'name': eval(data)['name'],
+            'drink_name': eval(data)['drink_name']
+        }
+        r.set(str(real_data),tiempo_demora.total_seconds())
+        f.write(str(tiempo_demora.total_seconds()) + '\n')
+    c.close()
+    f.close()
+        
+if __name__ == '__main__':
+    main()
